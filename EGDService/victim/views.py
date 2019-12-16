c5311427580645c3451de5eaf90c8e5e80fb1c1e
DELL <minhajul35-1863@diu.edu.bd>
2019-12-17 02:05:00 +0600

    Transfer Data

--- EGDService/victim/views.py
+++ EGDService/victim/views.py
@@ -4,26 +4,23 @@ from .models import Complain
 
 # Create your views here.
 
-def showvictim(request):
-    return render(request, "index.html")
-
-
-def loginPage(request):
-    return render(request, "login.html")
-
-
 def userPage(request):
     return render(request, "user_dashboard.html")
 
 
+#def call_complain(request):
+ #   return render(request, "u_victim/complain.html")
+
+
 def userComplain(request):
     if request.method == 'POST':
-        c = Complain(Complain_id=request.POST['Complain_id'], Complain_type=request.POST['Complaint_type'],
+        c = Complain(Complain_type=request.POST['Complaint_type'],
                      Complainant_name=request.POST['name'], Mobile_Number=request.POST['mobile'],
                      National_id=request.POST['identity_id'], District=request.POST['District'],
                      Thana=request.POST['Thana'],
                      Address=request.POST['address'], Complain_description=request.POST['Y_complain'],
                      Email=request.POST['email'])
         c.save()
-    return render(request, "complain.html")
-
+        return render(request, "u_victim/complain.html")
+    else:
+        return render(request, "u_victim/complain.html")
