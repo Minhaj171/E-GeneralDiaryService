72e4e1686d73d3155845ff9009895a3def952d12
DELL <minhajul35-1863@diu.edu.bd>
2019-12-17 02:05:28 +0600

    Transfer Data

--- EGDService/Administrator/views.py
+++ EGDService/Administrator/views.py
@@ -1,6 +1,9 @@
+from django.contrib import messages
 from django.shortcuts import render
 from victim.models import Complain
 from tourist.models import PoliceVerification
+
+from .models import policeComplain
 
 
 def adminHome(request):
@@ -15,3 +18,20 @@ def showComplain(request):
 def ShowVerification(request):
     p = PoliceVerification.objects.all()
     return render(request, 'administrator/view_verifications.html', {'verificationlist': p})
+
+
+def sendComplain(request, id):  # send complain to police-2
+    sc = Complain.objects.get(id=id)
+    multiple_complain = policeComplain.objects.filter(id=id)
+    if multiple_complain:
+        messages.success(request, "This complain is already sent!")
+        return showComplain(request)
+    else:
+        policeComplain.objects.create(id=sc.id, Complain_type=sc.Complain_type,
+                                      Complainant_name=sc.Complainant_name, Mobile_Number=sc.Mobile_Number,
+                                      National_id=sc.National_id, District=sc.District,
+                                      Thana=sc.Thana,
+                                      Address=sc.Address, Complain_description=sc.Complain_description,
+                                      Email=sc.Email)
+        messages.success(request, "Complain Send to Police Successfully!")
+        return showComplain(request)
--- EGDService/policeOfficer/views.py
+++ EGDService/policeOfficer/views.py
@@ -1,14 +1,36 @@
+from django.contrib import messages
 from django.shortcuts import render
 
+from victim.models import Complain
 
-# Create your views here.
+from Administrator.models import policeComplain
+from .models import police
+
+
 def Show_officer_Dash(request):
     return render(request, "policeofficer/PDashBoard.html")
 
 
 def ShowCrimes(request):
-    return render(request, "policeofficer/crime.html")
+    po = police.objects.get(email=request.session.get('police_email'))
+    obj = policeComplain.objects.filter(Thana=po.thana)
+    context = {'complain': obj}
+    return render(request, "policeofficer/crime.html", context)
 
 
 def Show_ver_requests(request):
     return render(request, "policeofficer/verification_requests.html")
+
+def police_reg(request):
+    if request.method == 'POST':
+        check_thana = police.objects.filter(thana = request.POST['thana'])
+        if check_thana:
+            messages.success(request, "This Police is already registered!!")
+            return render(request, 'policeofficer/p_registration.html')
+        else:
+            police_create = police.objects.create(name=request.POST['pname'], email = request.POST['pemail'], password = request.POST['password'], phone = request.POST['phone'],thana = request.POST['thana'])
+            police_create.save()
+            messages.success(request, "Police Account Created!")
+            return  render(request, 'policeofficer/p_registration.html')
+    else:
+        return  render(request, 'policeofficer/p_registration.html')
